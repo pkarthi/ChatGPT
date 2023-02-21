d986e50592a33fd110139c5cec87a45212b17ae9
Karthi P <karthi.perumal@concentrix.com>
2023-02-21 23:51:31 +0530

    final updates - working code

--- api/index.js
+++ api/index.js
@@ -56,11 +56,11 @@ app.post("/", async (req, res) => {
 
   let operandSearch = await runIndex(message);
 
-  const basePromptPrefix = `This is a conversation between the YouTuber Siraj Raval and a stranger.\nRelevant information that Siraj knows:\n${operandSearch}`;
+  const basePromptPrefix = `This is a conversation between the me Karthi and a stranger.\nRelevant information that Karthi knows:\n${operandSearch}`;
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${basePromptPrefix}\n\nStranger:${message}\n\nSiraj:`,
+    prompt: `${basePromptPrefix}\n\nStranger:${message}\n\nKarthi:`,
     max_tokens: 256,
     temperature: 0.7,
   });
